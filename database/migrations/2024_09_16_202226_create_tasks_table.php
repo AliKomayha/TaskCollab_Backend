6f101a7b01662386945412c4ca79b94013bc7b40
technologic <allikomayha789@gmail.com>
2024-09-16 23:34:21 +0300

    creating migration for tasks table

---
+++ database/migrations/2024_09_16_202226_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('manager_id');
+            $table->unsignedBigInteger('employee_id');
+            $table->string('content');
+            $table->date('due_date');
+            $table->enum('status',['pending','in progress','completed'])->default('pending');
+            
+            $table->foreign('manager_id')->references('id')->on('users');
+            $table->foreign('employee_id')->references('id')->on('users');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
